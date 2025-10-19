6b27026ebaefcbd7f6fa22fda1f2d4a2149998a3
Chris McLoughlin <cmcloug@purdue.edu>
2025-10-18 20:51:33 -0400

    All files (unorganized) but uploaded!

---
+++ api.js
@@ -0,0 +1,14 @@
+const API_KEY = "83886bb94a975f3fd6248c4e24cbd619";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+};
