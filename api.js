ab5e0898f692de4ce237de49321bc9e858ea4fac
Chris McLoughlin <cmcloug@purdue.edu>
2025-10-20 09:39:39 -0400

    Update api.js
    Updating api key

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const API_KEY = "83886bb94a975f3fd6248c4e24cbd619";
+const API_KEY = "insert key here";
 const BASE_URL = "https://api.themoviedb.org/3";
 
 export const getPopularMovies = async () => {
@@ -11,4 +11,5 @@ export const searchMovies = async (query) => {
     const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
     const data = await response.json();
     return data.results;
+
 };
